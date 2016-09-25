c84edd0449645527fc6ed4bec08bf5eff6837a43
Jack Wiebe <jack.b.wiebe@gmail.com>
2016-09-25 12:23:36 -0400

    Ramps and tings

--- DESN2013_M1_Wiebe_Jack_CarCombatGame/Assets/Fire.cs
+++ DESN2013_M1_Wiebe_Jack_CarCombatGame/Assets/Fire.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class Fire : MonoBehaviour {
 
+	public float flickerTime;
+	[SerializeField]
+	private ParticleSystem part;
+	private BoxCollider col;
+
 	// Use this for initialization
 	void Start () {
-	
+		part = this.GetComponent<ParticleSystem> ();
+		col = this.GetComponent<BoxCollider> ();
+		StartCoroutine (Flicker ());
 	}
 
 	void OnTriggerEnter(Collider col)
@@ -20,5 +27,23 @@ public class Fire : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public IEnumerator Flicker()
+	{
+		bool isOn = false;
+		while(true)
+		{
+			yield return new WaitForSeconds (flickerTime);
+			if (!isOn) {
+				col.enabled = true;
+				part.Play ();
+				isOn = true;
+			} else {
+				part.Stop ();
+				isOn = false;
+				col.enabled = false;
+			}
+		}
 	}
 }
